281589aa560edddb84b45060b66d9e66c73a7730
lucask <kummer.lucas@gmail.com>
2020-08-12 16:00:49 +0200

    lk: edited App.js to make CI build pass

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  return (<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>);
 }
 
 export default App;
