24d1e0f5d6750bfb6b20f9fc726c9570338e27cb
lucask <kummer.lucas@gmail.com>
2020-08-12 22:27:19 +0200

    lk: make tests pass again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { Wrapper } from './components/Wrapper';
-import { Navbar } from './components/Navbar';
-import { Home } from './pages/Home';
-import { Contact } from './pages/Contact';
-import { About } from './pages/About';
-import { useRoutes } from 'hookrouter';
-import { NotFound } from './pages/NotFound';
+import React from "react";
+import { Wrapper } from "./components/Wrapper";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { About } from "./pages/About";
+import { useRoutes } from "hookrouter";
+import { NotFound } from "./pages/NotFound";
 
 const routes = {
-  '/': () => <Home />,
-  '/contact': () => <Contact />,
-  '/about*': () => <About />
-}
+  "/": () => <Home />,
+  "/contact": () => <Contact />,
+  "/about*": () => <About />,
+};
 
 function App() {
-  const match = useRoutes(routes)
-  return (
-  <Wrapper>
-    <Navbar />
-    {match || <NotFound />}
-  </Wrapper>
-    );
+  const match = useRoutes(routes);
+  return (<Wrapper>
+      <Navbar />
+      {match || <NotFound />}
+    </Wrapper>);
 }
 
 export default App;
